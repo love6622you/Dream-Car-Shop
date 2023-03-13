53ab1f8a3e1890fc79a3583320b5f809dd529c71
Chia <chia.chen@unionfly.tech>
2023-03-13 11:12:34 +0800

    init

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import { Image } from "@chakra-ui/react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <Image />
+    </div>
+  );
+};
+
+export default Header;
