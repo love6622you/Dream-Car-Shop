eca9c703bb9e6e3303c905c63594072b4cc893c2
Chia <chia.chen@unionfly.tech>
2023-03-15 16:30:35 +0800

    [MOD]Slider width

--- src/pages/home/Comments.tsx
+++ src/pages/home/Comments.tsx
@@ -15,16 +15,23 @@ import comments from "@/assets/data/comment.json";
 
 export default function Comments() {
   const [slider, setSlider] = React.useState<Slider | null>(null);
-  const top = useBreakpointValue({ base: "95%", md: "50%" });
-  const side = useBreakpointValue({ base: "30%", md: "0px" });
+  const top = useBreakpointValue({
+    base: "95%",
+    md: "50%"
+  });
+  const side = useBreakpointValue({
+    base: "35%",
+    md: "1%"
+  });
 
   // Settings for the slider
   const settings = {
     dots: false,
-    autoplay: false,
-    speed: 300,
-    autoplaySpeed: 5000,
-    slidesToShow: useBreakpointValue({ base: 1, lg: 3 }),
+    arrows: false,
+    autoplay: true,
+    speed: 100,
+    autoplaySpeed: 3000,
+    slidesToShow: useBreakpointValue({ base: 1, md: 2, xl: 3 }),
     slidesToScroll: 1
   };
 
@@ -52,13 +59,15 @@ export default function Comments() {
       <Heading fontSize={"3xl"} m={"1.875rem"} textAlign={"center"}>
         What Clients Says
       </Heading>
+
       <Box
         position={"relative"}
         maxW={"100vw"}
-        height={{ base: "450px", lg: "400px" }}
+        height={{ base: "480px", md: "400px" }}
         overflow={"hidden"}
         mx={"auto"}
         color={"#718096"}
+        px={{ base: 4, md: "1.375rem", lg: 5 }}
       >
         <CarouselsArrowIcon
           config={{
@@ -78,22 +87,25 @@ export default function Comments() {
         {/* Slider */}
         <Slider {...settings} ref={(slider) => setSlider(slider)}>
           {comments.map((info, index) => (
-            <Box key={index}>
+            <Box key={index} px={2}>
               <Stack
-                w={{ base: "384px", lg: "440px" }}
-                h={{ base: "374px", lg: "400px" }}
+                w={"full"}
+                maxW={{ base: "480px", lg: "460px" }}
+                h={{ base: "auto", md: "400px" }}
                 bgColor={"#FFF"}
-                p={10}
-                pb={12}
+                p={{ base: "1.5rem 1.5rem 2rem", md: "2.5rem 2.5rem 3rem" }}
                 rounded={"lg"}
                 mx={"auto"}
+                spacing={5}
+                overflow={"scroll"}
               >
-                <Text pt={5} flexGrow={1}>
+                <Text px={6} pt={{ base: 5 }}>
                   {info.comment}
                 </Text>
 
                 <Flex alignItems={"center"}>
                   <Avatar boxSize={"3.75rem"} rounded={"full"} mr={4} />
+
                   <Box alignItems={"flex-start"}>
                     <Text>Happy Customer</Text>
                     <Text as={"span"} pr={2} color={"#2D3748"}>
